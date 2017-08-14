5cab45995a9c428eb5e0578d229573caa430df9f
Jan Kinsky <jan.a.kinsky@gmail.com>
2017-08-14 17:49:26 +0200

    created controllers and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,22 @@
 Rails.application.routes.draw do
+  get 'reviews/create'
+
+  get 'bookings/new'
+
+  get 'bookings/create'
+
+  get 'services/index'
+
+  get 'services/show'
+
+  get 'services/new'
+
+  get 'services/create'
+
+  get 'services/edit'
+
+  get 'services/update'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
