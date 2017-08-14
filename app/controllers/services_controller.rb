d512f4e4899857f8efae59ed17b228a0fc994a88
Jan Kinsky <jan.a.kinsky@gmail.com>
2017-08-14 18:23:04 +0200

    refactored routes

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,4 +1,6 @@
 class ServicesController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 Rails.application.routes.draw do
-  get 'reviews/create'
-
-  get 'bookings/new'
-
-  get 'bookings/create'
-
-  get 'services/index'
-
-  get 'services/show'
-
-  get 'services/new'
-
-  get 'services/create'
-
-  get 'services/edit'
-
-  get 'services/update'
 
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :services, only: [ :index, :show, :new, :create, :edit, :update] do
+    resources :bookings, only: [ :new, :create ]
+  end
+
+  resources :bookings, only: [] do
+    resources :reviews, only: [:create]
+  end
+
 end
