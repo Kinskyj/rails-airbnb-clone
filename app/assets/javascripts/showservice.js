a251b5adb7e0648ae4a5950a40aed32489cb7df2
Yoan Dorny <y.n.dorny@gmail.com>
2017-08-16 13:43:31 +0200

    done

--- app/assets/javascripts/showservice.js
+++ app/assets/javascripts/showservice.js
@@ -1,4 +1,4 @@
-let boxComment = document.getElementById("two").offsetHeight;
+const boxComment = document.getElementById("two").offsetHeight;
 
 $(function() {
   $("#one").sticky({bottomSpacing: 90 + boxComment});
