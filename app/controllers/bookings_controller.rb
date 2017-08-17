62bc3db68c9303317470b684fc0dfd432dda1f22
jeremy jodeau <jeremy.jodeau@gmail.com>
2017-08-17 19:01:08 +0200

    bug fixed on datetime"

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -6,20 +6,23 @@ class BookingsController < ApplicationController
   end
 
   def create
+    start_time = DateTime.strptime(params[:booking][:date_start], "%m/%d/%Y %l:%M %p")
     @booking = Booking.new(booking_params)
+    @booking.date_start = start_time
     @booking.user = current_user
     @booking.status = "pending"
     @booking.service = @service
     if @booking.save
       redirect_to service_path(@booking.service)
     else
+      raise
       render :new
     end
   end
 
   private
   def booking_params
-    params.require(:booking).permit(:date_start, :nb_hour, :status, :service_id)
+    params.require(:booking).permit(:nb_hour, :status, :service_id)
   end
 
   def set_service()
